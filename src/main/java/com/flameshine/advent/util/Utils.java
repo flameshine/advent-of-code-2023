59c4a6c88725cd67848b1ac422684e92df1d08e0
Reichemann <excellenceissoon@gmail.com>
2023-12-10 23:15:38 +0200

    Day 5 (part 1) completed

--- src/main/java/com/flameshine/advent/util/Utils.java
+++ src/main/java/com/flameshine/advent/util/Utils.java
@@ -30,4 +30,12 @@ public final class Utils {
             throw new IllegalStateException("Unable to parse integer from input string: " + s, e);
         }
     }
+
+    public static Long parseLong(String s) {
+        try {
+            return Long.parseLong(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalStateException("Unable to parse long from input string: " + s, e);
+        }
+    }
 }
